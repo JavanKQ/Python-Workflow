b6b3b4327cb0a6b1a5953f2b0c680cdd7a60f92e
Javan Quiroz <javan.quiroz@cosmoscorp.com>
2025-10-24 08:42:42 -0500

    Quick commit

--- scripts/cosmosdbquery.py
+++ scripts/cosmosdbquery.py
@@ -9,13 +9,28 @@ conn_str = (
     "Encrypt=yes;"
     "TrustServerCertificate=no;"
 )
-request_type = input("Enter the Request Type, Job Material Lot or Item Material List: ").lower
+while True:
+    request_type = input("Enter the Request Type, Job Material Lot or FG Material List: ").strip().lower()
+    if request_type in ["job material lot", "fg material list"]:
+        break
+    else:
+        print("Invalid input. Please enter 'Job Material Lot' or 'FG Material List'.")
 
 if request_type == "job material lot":
-    job_number = input("Enter the Job Number (e.g. JM00009456): ").upper()
+    while True:
+        job_number = input("Enter the Job Number (e.g. JM00009456): ").strip().upper()
+        if job_number.startswith("JM") and len(job_number) == 10:
+            break
+        else:
+            print("Invalid Job Number format. Please ensure it starts with 'JM' and is 10 characters long.")
 
-elif request_type == "item material list":
-    item_number = input("Enter the Item Number: ").upper()
+elif request_type == "fg material list":
+    while True:
+        item_number = input("Enter the Item Number: ").strip().upper()
+        if item_number:
+            break
+        else:
+            print("Item Number cannot be empty. Please enter a valid Item Number.")
 else:
     print("Invalid Request Type")
 
@@ -23,7 +38,27 @@ with pyodbc.connect(conn_str) as conn:
     if request_type == "job material lot":
         query = f"""
         SELECT item, lot
-        FROM ERP_Reporting.matltran_mst 
-        WHERE [ref_num] = '{job_number}'
-            AND [lot] <> '';
+        FROM ERP_Reporting.matltran_mst
+        WHERE [ref_num] COLLATE SQL_Latin1_General_CP1_CI_AS = '{job_number}'
+        AND [lot] <> '';
         """
+        df=pd.read_sql(query, conn)
+        print(f"Here are the material lots for {job_number}:")
+        print(df)
+    elif request_type == "fg material list":
+        query = f"""
+        ;WITH FirstRef AS (
+            SELECT TOP 1 ref_num
+            FROM ERP_Reporting.matltran_mst
+            WHERE [item] COLLATE SQL_Latin1_General_CP1_CI_AS = '{item_number}'
+              AND [trans_type] = 'F'
+            ORDER BY ref_num
+     )
+        SELECT DISTINCT m.[item]
+        FROM ERP_Reporting.matltran_mst AS m
+        INNER JOIN FirstRef f ON m.ref_num = f.ref_num
+        WHERE m.[trans_type] = 'I';
+        """
+        df=pd.read_sql(query, conn)
+        print(f"Here is the material list for {item_number}:")
+        print(df)
